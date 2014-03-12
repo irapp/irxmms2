18a72de7696e024a4e2b521bc81f452a5574a517
Clumart.G <myregs6@gmail.com>
2014-03-13 02:03:21 +0800

    add log support

--- README.md
+++ README.md
@@ -2,11 +2,13 @@ README
 =======================================
 1.Introduction
 
-apt-get install lirc liblircclient-dev  
+apt-get install lirc liblircclient-dev xmms2 xmms2-dev libxmmsclient-dev  
+cd /usr/include && ln -s xmms2/* ./  
 wget https://github.com/irapp/irxmms2/archive/master.zip  
 unzip master.zip  
-gcc -o irxmms2 -llirc_client irxmms2.c  
-
+gcc -o irxmms2 -llirc_client -lxmmsclient irxmms2.c  
+xmms2d
+./irxmms2 /etc/lirc/lircrc_irxmms2  
 
 2.Update
 
--- irxmms2.c
+++ irxmms2.c
@@ -16,24 +16,99 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
+#include <syslog.h>
 #include <lirc/lirc_client.h>
+#include <xmmsclient/xmmsclient.h>
 
-char *progname;
+#define TRUE 1
+#define FALSE 0
+#define RETURN_SUCCESS 1
+#define RETURN_FAILE 2
+
+static int connect_status = FALSE;  //save the status of xmms2's connection
+
+void xmms2_disconnect_callback(void *userdata)
+{
+    syslog(LOG_ERR,"Error: Code:209.\n");
+    syslog(LOG_INFO,"XMMS2 close the connection!\n");
+    connect_status = FALSE;
+    exit(EXIT_FAILURE);
+}
+
+static int xmms2_conntion(char *program_name)   //Connect To Xmms2 
+{
+    xmmsc_connection_t *xmms2conn = NULL;
+    char *xmms2_socket_path = NULL;
+    int  result = RETURN_FAILE;
+
+    //get socket of xmms2
+    xmms2_socket_path = getenv("XMMS_PATH");
+
+    //Init the xmms connection
+    xmms2conn = xmmsc_init(program_name);
+
+    if( xmms2conn == NULL )
+    {   //init failed and quit
+        syslog(LOG_ERR,"Error: Code:102.\n");
+        syslog(LOG_INFO,"XMMS2 API init failed! Maybe your memory not enough!\n");
+        result = RETURN_FAILE;
+        exit(EXIT_FAILURE);
+    }
+    else
+    {   //init success ,then connect to xmms2's socket
+        if( !xmmsc_connect(xmms2conn,xmms2_socket_path) )
+        {   //xmms2 connect failed.
+            char* error_message;
+            error_message = xmmsc_get_last_error( xmms2conn );
+            syslog(LOG_ERR,"Error: Code:103.\n");
+            syslog(LOG_INFO,"XMMS2 connection is failed!\n");
+            syslog(LOG_DEBUG,"%s\n",error_message);
+            syslog(LOG_DEBUG,"GET XMMS2 SOCKET PATH: %s \n",xmms2_socket_path);
+            result = RETURN_FAILE;
+            exit(EXIT_FAILURE);
+        }
+        else
+        {   //xmms2 connect success,then update the connect_status and set callback.
+            syslog(LOG_INFO,"Connect xmms2d success!\n");
+            xmmsc_disconnect_callback_set(xmms2conn, xmms2_disconnect_callback, NULL);
+            connect_status = TRUE;
+            result = RETURN_SUCCESS;
+        }
+    }
+    return result;                                                                  
+}
 
 int main(int argc, char *argv[])    //argc为命令和命令行参数的总数,argv[0]为程序名，argv[1]为第一个参数
 {
+    char *program_name;
     struct lirc_config *config;
-    progname=argv[0];   //得到程序本身的名字
+
+    program_name="irxmms2";   //得到程序本身的名字,fix me ,here can get name by argv[0]and strformat
+
+    //显示程序基础信息
+    printf("Welcome To Use IRxmms2 :) \n");
+    printf("Version is : %s \n", VERSION);
+    printf("Logs in /var/log/user.log \n");
+    
+    openlog(program_name,LOG_PID|LOG_ODELAY|LOG_CONS,LOG_USER);
     if(argc>2)          //如果有一个以上的参数，说明有问题，程序退出
     {
-        fprintf(stderr,"Usage: %s <config file>\n",progname);
+        fprintf(stderr,"Usage: %s <config file>\n",program_name);
+        syslog(LOG_ERR,"Usage: %s <config file>\n",program_name);
         exit(EXIT_FAILURE);
     }
-
+    
+    xmms2_conntion(program_name);
     //初始化连接lircd，如果连接成功，什么都不打印，如果连接失败，则在错误终端返回第二个参数的值(非0)
     //该函数正常返回lircd的socket的文件描述符，错误情况下返回-1
-    if(lirc_init("irxmms2",10)==-1) exit(EXIT_FAILURE); 
+    if(lirc_init(program_name,10)==-1)
+    {
+        syslog(LOG_ERR,"Error: Code:101.\n");
+        syslog(LOG_INFO,"Lirc API init is failed! Maybe your memory not enough!\n");    
+        exit(EXIT_FAILURE); 
+    }
 
     //lirc_readconfig(char *file,struct lirc_config **config,int (check)(char *s));
     //该函数加载配置,可以多次执行以加载多个配置;该函数执行成功返回0，失败返回-1;
@@ -59,16 +134,18 @@ int main(int argc, char *argv[])    //argc为命令和命令行参数的总数,a
             //string就是配置文件里面config字段的部分
             //因为用户也许会一个键按多次，因此必须要等到NULL字段的返回(即用户活动停止)或者产生了错误再继续
             //该函数正常返回0，错误返回-1
-            while((ret=lirc_code2char(config,code,&c))==0 &&
-                  c!=NULL)
+            while((ret=lirc_code2char(config,code,&c))==0 && c!=NULL)
             {
 #ifdef DEBUG
                 if (strcmp(c,"play") == 0)
-                    printf("Execing command \"%s\"\n",c);
+                    printf("Hello \"%s\"\n",c);
                 else if (strcmp(c,"KEY_1") == 0)
-                    printf("Execing command \"%s\"\n",c);
+                    printf("Hello \"%s\"\n",c);
                 else
-                    printf("Execing command \"%s\"\n",c);
+                    printf("Hello \"%s\"\n",c);
+
+                sleep(1);
+                c = NULL;
 #endif
             }
             free(code);
@@ -79,5 +156,6 @@ int main(int argc, char *argv[])    //argc为命令和命令行参数的总数,a
     }
     //int lirc_deinit();该函数断开和lircd的连接并进行内部的清理
     lirc_deinit();
+    closelog();
     exit(EXIT_SUCCESS);
 }
